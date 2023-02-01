c37b889f7863a9a0dbd6c15cc115230f5e03910c
zhangbofei <zhangbofei@163.com>
2023-02-01 11:33:14 +0800

    添加参考引用文章组件

---
+++ src/components/ReferSource/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Reference: React.FC<{ pluginList: Array<[string, string]>, inspiration: string }> = ({ pluginList, inspiration }) => {
+  return (<div>
+    项目灵感来源：URL
+    项目使用插件地址
+  </div>)
+}
+
+export default Reference
