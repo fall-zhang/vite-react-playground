c8ea8b42cb9e87a56faf22ce8fe880b28e134413
fall <zhangbofei@163.com>
2023-11-15 15:20:02 +0800

    更新时钟组件页面

--- src/pages/clock/OneClock.tsx
+++ src/pages/clock/OneClock.tsx
@@ -1,39 +1,44 @@
 import styles from './OneClock.module.less'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 const OneClock: React.FC = () => {
-  const [hour, setHour] = useState(0)
-  const [minute, setMinute] = useState(0)
-  const [second, setSecond] = useState(0)
+  const initTime = new Date()
+  const [hour, setHour] = useState(initTime.getHours())
+  const [minute, setMinute] = useState(initTime.getMinutes())
+  const [second, setSecond] = useState(initTime.getSeconds())
+  const c_hour = useRef<HTMLDivElement>(null)
+  const c_minuit = useRef<HTMLDivElement>(null)
+  const c_second = useRef<HTMLDivElement>(null)
+  const clock = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    const c_hour = document.getElementById('hour')
-    const c_minuit = document.getElementById('minute')
-    const c_second = document.getElementById('second')
     const interval = setInterval(setTime, 500)
     function setTime() {
-      const time = new Date
+      const time = new Date()
       setHour(time.getHours())
       setMinute(time.getMinutes())
       setSecond(time.getSeconds())
-      if (c_second) {
-        c_second.style.transform = 'rotate(' + (6 * second) + 'deg)'
-        c_second.style.display ='block'
+      if (clock.current) {
+        clock.current.style.display = 'block'
       }
-      if (c_minuit) {
-        c_minuit.style.transform = 'rotate(' + (6 * (minute + (second / 60))) + 'deg)'
-        c_minuit.style.display ='block'
+      if (c_second.current) {
+        c_second.current.style.transform = 'rotate(' + (6 * second) + 'deg)'
+        c_second.current.style.display = 'block'
       }
-      if (c_hour) {
-        c_hour.style.transform = 'rotate(' + (6 * (hour + (minute / 60))) + 'deg)'
-        c_hour.style.display ='block'
+      if (c_minuit.current) {
+        c_minuit.current.style.transform = 'rotate(' + (6 * (minute + (second / 60))) + 'deg)'
+        c_minuit.current.style.display = 'block'
+      }
+      if (c_hour.current) {
+        c_hour.current.style.transform = 'rotate(' + (6 * (hour + (minute / 60))) + 'deg)'
+        c_hour.current.style.display = 'block'
       }
     }
     return function () {
       clearInterval(interval)
     }
   }, [second])
+  const ul_neddle = useRef<HTMLUListElement>(null)
   useEffect(() => {
-    const ul_neddle = document.querySelector('.' + styles.neddle)
-    if (!ul_neddle?.childNodes.length) {
+    if (!ul_neddle.current?.childNodes.length) {
       for (let i = 0; i < 60; i++) {
         const liElement = document.createElement('li')
         liElement.className = styles.neddle_one
@@ -43,20 +48,20 @@ const OneClock: React.FC = () => {
           liElement.style.width = '5px'
         }
         if (ul_neddle) {
-          ul_neddle.appendChild(liElement)
+          ul_neddle.current?.appendChild(liElement)
         }
       }
     }
   }, [])
   return (
     <div className={styles.container}>
-      <div className={styles.clock}>
-        <ul className={styles.neddle}>
+      <div className={styles.clock} ref={clock}>
+        <ul className={styles.neddle} ref={ul_neddle}>
         </ul>
         <div className={styles.clock_container}>
-          <div className={styles.hour} id="hour"></div>
-          <div className={styles.minute} id="minute"></div>
-          <div className={styles.second} id="second"></div>
+          <div className={styles.hour} ref={c_hour} id="hour"></div>
+          <div className={styles.minute} ref={c_minuit} id="minute"></div>
+          <div className={styles.second} ref={c_second} id="second"></div>
           <div className={styles.clock_center}></div>
         </div>
       </div>
